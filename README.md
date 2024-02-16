8eeb34f91a350e5f791f39bc5d1ce9c3e3383068
Ximo Rovira <jrovira@kumori.systems>
2024-02-16 10:00:22 +0100

    rew minimal repro

--- README.md
+++ README.md
@@ -3,20 +3,15 @@
 ## Dependencies
 
 - `docker`
-- `kind`
 
 ## How to run
 
 ```bash
-./run.sh; ./run_log.sh
+./run.sh
 ```
 
-You will eventually see a fast feed of logs from the KubeAPI server complaining about `illegal resource version from storage: 0` indefinately.
+The run script will guide you through the demonstration.
 
-For some reason it does not work. However under the `works-with-k3s` directory you will find an example of it working as expected.
+You will eventually see a fast feed of logs from the KubeAPI server complaining about `illegal resource version from storage: 0` indefinitely.
 
-## Clean up
-
-```bash
-kind delete cluster
-```
+For some reason KubeAPI is unable to initialize the datastore.
--- run.sh
+++ run.sh
@@ -1,15 +1,67 @@
 #!/usr/bin/env bash
-cd $(dirname "$0")
-NAME="${NAME:-kind}"
 
-cat kind-cluster.yaml.template | envsubst > kind-cluster.yaml
+clear
+cat <<- EOF
+----------------------------------------------------------------------------
 
-echo 
-echo 
-echo Cluster creation will time out. 
-echo You should cancel it during the \"Starting control-plane\" phase.
-echo 
-echo 
+We will now deploy NATS (v2.10.5), Kine (v0.11.4) and KubeAPI (v1.29.1)
+Once Kine is up and running we will se how KubeAPI prints out a stream of:
 
-trap "exit 0" SIGINT
-kind create cluster --config kind-cluster.yaml --name "$NAME" --retain
+      illegal resource version from storage: 0
+
+Wait for new instruction to appear.
+
+----------------------------------------------------------------------------
+EOF
+read -p "Press enter to continue"
+
+# Start NATS, Kine and KubeAPI
+docker compose up nats kine kubeapi -d
+
+# Watch how it fails
+docker compose logs -f kubeapi &
+PID=$!
+sleep 10
+kill -s SIGKILL $PID
+
+cat <<- EOF
+----------------------------------------------------------------------------
+
+We will now start K3s (v1.29.1-k3s2) disabling most components except the 
+KubeAPI server.
+
+It is configured to use the previous Kine instance as its datastore. After
+it initializes, we will kill it and restart KubeAPI.
+
+We will observe how the previous stream of error no longer appears.
+
+Wait for new instruction to appear.
+
+----------------------------------------------------------------------------
+EOF
+read -p "Press enter to continue"
+
+
+docker compose up k3s-kubeapi -d    # Start K3s to simulate a KubeAPI instance
+sleep 10                            # Wait for it to do its thing...
+docker compose down k3s-kubeapi     # Kill it (not strictly necessary)
+
+
+# Watch how it now works
+docker compose logs -f kubeapi &
+PID=$!
+sleep 10
+kill -s SIGKILL $PID
+
+
+cat <<- EOF
+----------------------------------------------------------------------------
+
+The error should no longer appear in the (Kubernetes) KubeAPI logs.
+
+THE END
+
+----------------------------------------------------------------------------
+EOF
+read -p "Press enter to clean up"
+docker compose down
